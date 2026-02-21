89c6cefa4e8d93d9a7014ee77edb3628bf90d197
Soney <sonephyo7777777@gmail.com>
2026-02-21 13:59:09 -0500

    feat: add mutex lock for handling concurrent request

--- router/loadbalancer/roundrobin/roundrobin.go
+++ router/loadbalancer/roundrobin/roundrobin.go
@@ -3,14 +3,20 @@ package roundrobin
 import (
 	"fmt"
 	"llm-routing-bench/router/backend"
+	"sync"
 )
 
 type RoundRobin struct {
 	curBackend int
+	mu         sync.Mutex
 }
 
 func (rb *RoundRobin) Route(backends []backend.Backend) backend.Backend {
+	rb.mu.Lock()
+	defer rb.mu.Unlock()
+	
 	fmt.Println("This is RoundRobin")
+
 	idx := rb.curBackend % len(backends)
 	rb.curBackend++
 	return backends[idx]
