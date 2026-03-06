0ccf02f65e0346e46488498d195265589e11ba51
Soney <sonephyo7777777@gmail.com>
2026-03-06 00:21:32 -0500

    chore: add logs for debug

--- router/loadbalancer/leastqueue/leastqueue.go
+++ router/loadbalancer/leastqueue/leastqueue.go
@@ -3,7 +3,6 @@ package leastqueue
 import (
 	"llm-routing-bench/router/backend"
 	"llm-routing-bench/router/scraper"
-	"log"
 	"net/http"
 )
 
@@ -21,6 +20,5 @@ func (lq *LeastQueue) Route(r *http.Request) *backend.Backend {
 	for i := range lq.backends {
 		scraper.GetFilteredMetrics(lq.backends[i].BackendURI, []string{"vllm:num_requests_running"})
 	}
-	log.Println()
 	return &lq.backends[0]
 }
--- router/scraper/scraper.go
+++ router/scraper/scraper.go
@@ -1,7 +1,9 @@
 package scraper
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -9,27 +11,44 @@ import (
 )
 
 func GetFilteredMetrics(url string, keep []string) {
+
+	log.Println("CP 1")
 	resp, err := http.Get(url + "/metrics")
+	log.Println(url + "/metrics")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		log.Fatalf("unexpected status %d from %s/metrics: %s", resp.StatusCode, url, string(body))
+	}
+
+	log.Println("CP 2")
+	body, _ := io.ReadAll(resp.Body)
+	log.Printf("raw body: %.500s", string(body))    // first 500 chars
+	resp.Body = io.NopCloser(bytes.NewReader(body)) // restore for parser
 	var parser expfmt.TextParser
+	log.Println(resp.Body)
 	mf, err := parser.TextToMetricFamilies(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	log.Println("CP 3")
 	keepSet := make(map[string]bool)
 	for _, name := range keep {
 		keepSet[name] = true
 	}
 
+	log.Println("CP 4")
 	for name, family := range mf {
 		if !keepSet[name] {
 			continue
 		}
+
+		log.Println("CP 5")
 		for _, m := range family.GetMetric() {
 			fmt.Printf("%s %v\n", name, m.GetGauge().GetValue())
 		}
