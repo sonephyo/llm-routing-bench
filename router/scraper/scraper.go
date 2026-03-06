e14518919c6e0e53d758cfa87f9d821050333781
Soney <sonephyo7777777@gmail.com>
2026-03-06 00:28:54 -0500

    fix: add model.UTF8Validation
    - https://github.com/kubernetes/kubernetes/issues/133878

--- router/scraper/scraper.go
+++ router/scraper/scraper.go
@@ -8,13 +8,12 @@ import (
 	"net/http"
 
 	"github.com/prometheus/common/expfmt"
+	"github.com/prometheus/common/model"
 )
 
 func GetFilteredMetrics(url string, keep []string) {
 
-	log.Println("CP 1")
 	resp, err := http.Get(url + "/metrics")
-	log.Println(url + "/metrics")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,30 +24,22 @@ func GetFilteredMetrics(url string, keep []string) {
 		log.Fatalf("unexpected status %d from %s/metrics: %s", resp.StatusCode, url, string(body))
 	}
 
-	log.Println("CP 2")
-	body, _ := io.ReadAll(resp.Body)
-	log.Printf("raw body: %.500s", string(body))    // first 500 chars
-	resp.Body = io.NopCloser(bytes.NewReader(body)) // restore for parser
-	var parser expfmt.TextParser
-	log.Println(resp.Body)
+	parser := expfmt.NewTextParser(model.UTF8Validation)
 	mf, err := parser.TextToMetricFamilies(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("CP 3")
 	keepSet := make(map[string]bool)
 	for _, name := range keep {
 		keepSet[name] = true
 	}
 
-	log.Println("CP 4")
 	for name, family := range mf {
 		if !keepSet[name] {
 			continue
 		}
 
-		log.Println("CP 5")
 		for _, m := range family.GetMetric() {
 			fmt.Printf("%s %v\n", name, m.GetGauge().GetValue())
 		}
